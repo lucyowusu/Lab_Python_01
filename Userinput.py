cdc23dd7b01d4306f7c94ef5a7e010b51e50a5a6
AITI Student <aiti-student@AITI-74.(none)>
2012-06-20 13:14:29 +0000

    exercise 4, day2

---
+++ Userinput.py
@@ -0,0 +1,20 @@
+print 'Exercise 4'
+print ''
+
+fName =raw_input('Please enter your FirstName: ')
+
+lName =raw_input('Please enter your LasttName: ')
+
+print ''
+print ''
+
+print "Please enter your Date of Birth: "
+print ''
+month =raw_input('\tMonth? ')
+print ''
+Day =raw_input('\tDay? ')
+print ''
+Year =raw_input('\tYear? ')
+print ''
+print ''
+print fName,lName, "was born on", month,Day +',',Year
