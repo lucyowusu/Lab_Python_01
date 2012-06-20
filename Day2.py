e23ebe993ccedc5ce49f89ecb2e58a2cb3eadde9
AITI Student <aiti-student@AITI-74.(none)>
2012-06-20 12:35:19 +0000

    Lab Exercises 1-3 for Day 2

---
+++ Day2.py
@@ -0,0 +1,94 @@
+print 'Exercise 1'
+print ''
+print 'Variable Names'
+
+print 'x is valid.'
+print ''
+print '56thnumber is not valid because it starts with a number.'
+print ''
+print 'length is valid.'
+print ''
+print '!Tayo! is not valid because it starts with a character.'
+print ''
+print 'NUMBER is valid.'
+print ''
+print 'Android_phone1 is valid.'
+print ''
+print 'this variable is not valid because it contains a space.'
+
+
+print''
+print''
+
+
+print ' 2. Types'
+print''
+print 'a=False is boolean.'
+print''
+print 'b=3.7 is a float.'
+print''
+print 'c="Alex" is a string.'
+print''
+print 'd=7 is an integer.'
+print''
+print 'e="True" is a string.'
+print''
+print 'f=17 is an integer.'
+print''
+print 'g="17" does not have a data type.'
+print''
+print 'h=True is boolean.'
+print''
+print 'i="3.14159" is a string.'
+print''
+print 'j is also a string.'
+
+
+
+
+print''
+print''
+print'Exercise 2'
+print''
+print'1'
+print''
+print'1.','b and c is','False'
+print''
+print'2.','b or c is', 'True'
+print''
+print'3.','not a and not b is', 'False'
+print''
+print'4.','(a and b) or not c is','True'
+print''
+print'5.','not b and not (a or c )is','False'
+print''
+print'6.','not((not b or not a) and c )is','True'
+
+ 
+print''
+print''
+print'Exercise 3'
+print''
+print'1.','5/2 is',2
+print''
+print'2.','5/2.0 is', 2.5
+print''
+print'3.','5.0/2 is', 2.5
+print''
+print'4.','7*(1/2)is',0
+print''
+print'5.','7*(1/2.0)is',3.5
+print''
+print'6.','5**2 is',25
+print''
+print'7.','5.0**2 is',25.00
+print''
+print'8.','5**2.0 is',25.00
+print''
+print'9.','1/3 is',0
+print''
+print'10.','1/3.0 is',0.3333333333333333
+
+
+
+
